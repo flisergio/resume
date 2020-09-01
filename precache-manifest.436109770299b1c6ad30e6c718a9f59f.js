03bb1008281c3350467b95e18c8cd5c9a5c3e62e
Sergio <sergey.dko@gmail.com>
2020-09-01 20:13:18 +0300

    Updates

---
+++ precache-manifest.436109770299b1c6ad30e6c718a9f59f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62d358bebe4574ebc6cb03cff2732b65",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "acb9d0548dc122581df7",
+    "url": "/resume/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bb206142cd59a12ed54b",
+    "url": "/resume/static/css/main.4378ce62.chunk.css"
+  },
+  {
+    "revision": "acb9d0548dc122581df7",
+    "url": "/resume/static/js/2.56340cea.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/resume/static/js/2.56340cea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb206142cd59a12ed54b",
+    "url": "/resume/static/js/main.12924ee2.chunk.js"
+  },
+  {
+    "revision": "2b17229f8e36d952a251",
+    "url": "/resume/static/js/runtime-main.6275bf14.js"
+  },
+  {
+    "revision": "d078968bc905921e539a6f954e1e2ca1",
+    "url": "/resume/static/media/background-about.d078968b.jpg"
+  },
+  {
+    "revision": "af7a29134941e0ce86b5c7bd613ba202",
+    "url": "/resume/static/media/background-contact.af7a2913.jpeg"
+  },
+  {
+    "revision": "3e03cc3f322255ee6a366bf7ad465595",
+    "url": "/resume/static/media/background-experience.3e03cc3f.jpg"
+  },
+  {
+    "revision": "fbc21ab90ac371e073cd8d29b73b1448",
+    "url": "/resume/static/media/background-links.fbc21ab9.jpg"
+  },
+  {
+    "revision": "ed68a967a5ebad03debb20c43f5aa7e8",
+    "url": "/resume/static/media/background-skills.ed68a967.png"
+  },
+  {
+    "revision": "37d615b821ae5933b7016b8d10a898bb",
+    "url": "/resume/static/media/education.37d615b8.svg"
+  },
+  {
+    "revision": "126f3bb30bc81b4abab897174fc309c8",
+    "url": "/resume/static/media/hobbies.126f3bb3.svg"
+  },
+  {
+    "revision": "fe6c79c36eebfb7f2a826830b7b9b8a7",
+    "url": "/resume/static/media/logo_main.fe6c79c3.png"
+  },
+  {
+    "revision": "c51405bfaa88685693efb449c4bb8dc4",
+    "url": "/resume/static/media/personal-info.c51405bf.svg"
+  },
+  {
+    "revision": "2cedbac047dc8b45700821ba8300f445",
+    "url": "/resume/static/media/profile.2cedbac0.JPG"
+  }
+]);
