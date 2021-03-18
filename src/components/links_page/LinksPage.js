cc249e250eb8e652ef42f992c746688a221bd95d
Sergio <sergey.dko@gmail.com>
2021-03-18 16:02:06 +0100

    resume - adding some links

--- src/components/links_page/LinksPage.js
+++ src/components/links_page/LinksPage.js
@@ -33,24 +33,95 @@ const LinksPage = ({ id }) => {
                     </div>
 
                     <div className="links-card_container">
-                        <div className="links_link-box_container" data-text="GitHub">
-                            <p>GitHub Profile</p>
+                        <div className="links_link-box_container" data-text="RoboFriends">
+                            <p>RoboFriends</p>
                         </div>
 
                         <div className="links_content_container">
                             <div className="links_content_inside_container">
                                 <p>
-                                    My main <span>GitHub</span> profile where I gather all my self-development projects or other projects, 
-                                    that can be shared. You can find there a <span>CultureCenter project</span>, which was a 
-                                    <span> university project</span> used for simple maintaining of the <span>cinema system</span>. It was
-                                    developed by me and two other friends in <span>Java</span> and <span>MySQL</span>.
+                                    Simple and one of my first <span>React.JS application</span>, that shows general way 
+                                    of <span>creating</span> React.JS <span>components</span>, has <span>searching functionality</span> and <span>scrolling window</span> component. 
+                                    Just a <span>practice</span> mixed with a <span>fun</span>, nothing more :)
                                 </p>
 
                                 <a 
                                     target="_blank" 
                                     rel="noopener noreferrer" 
-                                    href="https://github.com/flisergio"
-                                    className="link_button link_github">
+                                    href="https://flisergio.github.io/robofriends/"
+                                    className="link_button link_robofriends">
+                                        Visit Link
+                                </a>
+                            </div>                           
+                        </div>
+                    </div>
+
+                    <div className="links-card_container">
+                        <div className="links_link-box_container" data-text="FaceReco">
+                            <p>Face Recognition</p>
+                        </div>
+
+                        <div className="links_content_container">
+                            <div className="links_content_inside_container">
+                                <p>
+                                    <span>Front-End React.JS</span> application connected with a <span>Back-End (Node & Express.js)</span>. 
+                                    Application shows how simple <span>functionality of login/register</span> works in <span>NodeJS</span> and it is connected with 
+                                    open-source <span>Machine Learning</span> API that makes face recognition possible (only for <span>one-face images</span>).
+                                </p>
+
+                                <a 
+                                    target="_blank" 
+                                    rel="noopener noreferrer" 
+                                    href="https://flisergio.github.io/Face-Recognition-App/"
+                                    className="link_button link_facerecognition">
+                                        Visit Link
+                                </a>
+                            </div>                           
+                        </div>
+                    </div>
+
+                    <div className="links-card_container">
+                        <div className="links_link-box_container" data-text="Covid Cases">
+                            <p>Covid Cases</p>
+                        </div>
+
+                        <div className="links_content_container">
+                            <div className="links_content_inside_container">
+                                <p>
+                                    Simple <span>Static Web Page</span> that shows how <span>COVID-19 cases quantity was chaning</span> in 2020. 
+                                    Created with <span>Vanilla JS</span>. Can be <span>run</span> only <span>locally</span> in browser.
+                                </p>
+
+                                <a 
+                                    target="_blank" 
+                                    rel="noopener noreferrer" 
+                                    href="https://github.com/flisergio/covid-cases"
+                                    className="link_button link_facerecognition">
+                                        Visit Link
+                                </a>
+                            </div>                           
+                        </div>
+                    </div>
+
+                    <div className="links-card_container">
+                        <div className="links_link-box_container" data-text="Cinema">
+                            <p>Simple cinema system</p>
+                        </div>
+
+                        <div className="links_content_container">
+                            <div className="links_content_inside_container">
+                                <p>
+                                    <span>Back-End application</span>, which was a <span> university project</span> used for simple maintaining of 
+                                    the <span>cinema system</span>. It was developed by me and two other friends 
+                                    in <span>Java</span> on 2nd year of studies. Application is connected with a <span>MySQL</span> database. 
+                                    Needs Java to be installed for running the project.
+                                </p>
+
+                                <a 
+                                    target="_blank" 
+                                    rel="noopener noreferrer" 
+                                    href="https://github.com/flisergio/CultureCenter"
+                                    className="link_button link_culturecenter">
                                         Visit Link
                                 </a>
                             </div>  
